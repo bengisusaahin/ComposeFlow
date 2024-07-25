a503d4869d8fc727cbc91560c99624bdf1f6e0f8
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-07-25 12:48:31 +0300

    feat: Flow types

--- app/src/main/java/com/bengisusahin/composeflow/MyViewModel.kt
+++ app/src/main/java/com/bengisusahin/composeflow/MyViewModel.kt
@@ -1,8 +1,14 @@
 package com.bengisusahin.composeflow
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
@@ -57,4 +63,35 @@ class MyViewModel : ViewModel() {
              println(it)
          }.launchIn(viewModelScope)*/
     }
+
+    // Live Data  comparisons
+
+    // başka bir yerden bu live datamızı değiştiremeyiz
+    // initialize value vermek zorunda değiliz
+    private val _liveData = MutableLiveData<String>("KotlinLiveData")
+    val liveData : LiveData<String> = _liveData
+
+    fun changeLiveDataValue() {
+        _liveData.value = "Live Data"
+    }
+
+    // initialize value vermek zorundayız
+    // flowun objeleştirilmiş live dataya benzetilmiş hali
+    private val _stateFlow = MutableStateFlow("KotlinStateFlow")
+    val stateFlowValue = _stateFlow.asStateFlow()
+
+    // initialize value verdirtmiyo
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlowValue = _sharedFlow.asSharedFlow()
+
+    fun changeStateFlowValue() {
+        _stateFlow.value = "State Flow"
+    }
+
+    fun changeSharedFlowValue() {
+        viewModelScope.launch {
+            _sharedFlow.emit("Shared Flow")
+        }
+    }
+
 }

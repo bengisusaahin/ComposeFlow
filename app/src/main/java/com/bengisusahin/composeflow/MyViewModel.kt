71dc55e8ad30f37a3aa13e5d66b2f9fc614f70fe
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-07-25 12:10:17 +0300

    feat: flow operators

--- app/src/main/java/com/bengisusahin/composeflow/MyViewModel.kt
+++ app/src/main/java/com/bengisusahin/composeflow/MyViewModel.kt
@@ -1,8 +1,15 @@
 package com.bengisusahin.composeflow
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
 
@@ -16,5 +23,38 @@ class MyViewModel : ViewModel() {
             counter--
             emit(counter)
         }
+    }
+
+
+    init {
+        collectInViewModel()
+    }
+
+
+    private fun collectInViewModel() {
+        viewModelScope.launch {
+             countDownTimerFlow
+                 .filter {
+                     it %3 == 0
+                 }
+                 .map {
+                     it + it
+                 }
+                 .collect {
+                 println(it)
+             }
+
+            // delay yüzünden yeni bisi geldiğinde collect etmeden siliyor ve bekliyor ve en son işlemde ancak
+            // collect ediyor ama delay olmasaydı her yeni değer geldiğinde hemen işlem yapacaktı
+//            countDownTimerFlow.collectLatest {
+//                delay(2000)
+//                println("Counter is: $it")
+//            }
+        }
+
+
+        /* countDownTimerFlow.onEach {
+             println(it)
+         }.launchIn(viewModelScope)*/
     }
 }
